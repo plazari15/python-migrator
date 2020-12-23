dcb5faf7d42505d29928bd109f726006f46c04e6
Pedro Lazari <pedro.lazari@picpay.com>
2020-12-23 09:50:13 -0300

    Using the bgclass in this file

--- generator.py
+++ generator.py
@@ -2,17 +2,7 @@ from sys import exit, argv
 from datetime import datetime
 import os.path
 import shutil
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from bcolors import bcolors
 
 try:
     filename = argv[1]
@@ -27,19 +17,4 @@ if os.path.isfile(path_file):
     print(f"{bcolors.FAIL}Arquivo de migration já existe {bcolors.ENDC}")
     exit()
 
-#copy stub as class
 shutil.copy("stubs/blank.stub", path_file)
-#
-# f = open(path_file, 'rt')
-# data = f.read()
-# data = data.replace("CLASS_NAME", filename)
-# f.close()
-#
-# f = open(path_file, "wt")
-# f.write(data)
-# f.close()
-
-# for line in f:
-#     f.write(line.replace('CLASS_NAME', filename))
-
-# f.close()
