446e5b4689e04f1cf2d6e41487a803b4898c6e17
Pedro Lazari <pedro.lazari@picpay.com>
2020-12-26 12:00:19 -0300

    Feat: Prepare Generator to First Test

--- src/generator.py
+++ src/generator.py
@@ -4,17 +4,22 @@ import os.path
 import shutil
 from Helpers.bcolors import bcolors
 
-try:
-    filename = argv[1]
-except:
-    print(f"{bcolors.FAIL}Defina um nome para o arquivo! {bcolors.ENDC}")
-    exit()
+def generate_migration_file(migration_name):
+    timestamp_now = datetime.now().strftime("%Y_%m_%d_%H_%M_%s")
+    path_file = "migrations/%s_%s.py" % (timestamp_now, migration_name)
 
-timestamp_now = datetime.now().strftime("%Y_%m_%d_%H_%M_%s")
-path_file = "migrations/%s_%s.py" % (timestamp_now, filename)
+    if os.path.isfile(path_file):
+        print(f"{bcolors.FAIL}Arquivo de migration já existe {bcolors.ENDC}")
+        exit()
 
-if os.path.isfile(path_file):
-    print(f"{bcolors.FAIL}Arquivo de migration já existe {bcolors.ENDC}")
-    exit()
+    shutil.copy("src/stubs/blank.stub", path_file)
 
-shutil.copy("src/stubs/blank.stub", path_file)
+    return path_file
+
+if __name__ == '__main__':
+    try:
+        filename = argv[1]
+        generate_migration_file(filename)
+    except:
+        print(f"{bcolors.FAIL}Defina um nome para o arquivo! {bcolors.ENDC}")
+        exit()
