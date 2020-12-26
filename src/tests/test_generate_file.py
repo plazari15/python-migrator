5a7de228f9f3ef384ec444fdbf4e5e6bbbb0fb76
Pedro Lazari <plazari96@gmail.com>
2020-12-26 15:23:17 -0300

    Feat: Generator Migration

--- src/tests/test_generate_file.py
+++
@@ -1,7 +0,0 @@
-from src.generator import generate_migration_file
-import os.path
-
-def test_generate_migration_file():
-    file_name = generate_migration_file("file_test")
-    return os.path.isfile(file_name)
-
