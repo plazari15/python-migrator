da286b3842b8b1493ee926028e1a6c49e797fd4d
Pedro Lazari <plazari96@gmail.com>
2020-12-26 14:51:41 -0300

    Feat: TravisCI

--- src/generator.py
+++ src/generator.py
@@ -2,7 +2,17 @@ from sys import exit, argv
 from datetime import datetime
 import os.path
 import shutil
-from src.Helpers.bcolors import bcolors
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def generate_migration_file(migration_name):
     timestamp_now = datetime.now().strftime("%Y_%m_%d_%H_%M_%s")
