d7adbcbcb332ae68cf22385d39449b1172004e5f
Pedro Lazari <plazari96@gmail.com>
2020-12-26 15:43:44 -0300

    Feat: Generator Migration

---
+++ database_migration_repository.py
@@ -0,0 +1,66 @@
+from database_connector import mongo_db
+import pymongo
+
+database = mongo_db.connect()
+
+def create_migration_register(migration_name, batch_id):
+    database.migrations.insert_one({
+        "migration": migration_name,
+        "batch": batch_id
+    })
+
+def delete_migration_register(batch_id):
+    database.migrations.delete_many({
+        "batch": batch_id
+    })
+
+def check_file(migration_name):
+    counter = database.migrations.find({
+        "migration": migration_name
+    }).count()
+
+    if counter > 0:
+        return False
+
+    return True
+
+def get_batch_id():
+    last_register = list(database.migrations.find().sort("batch", pymongo.DESCENDING).limit(1))
+
+    if len(last_register) == 0:
+        return 1
+
+    if len(last_register) > 0:
+        return last_register[0]['batch'] + 1
+
+def get_last_batch():
+    last_register = list(database.migrations.find().sort("batch", pymongo.DESCENDING).limit(1))
+
+    if len(last_register) == 0:
+        return False
+
+    if len(last_register) > 0:
+        return last_register[0]['batch']
+
+
+def get_files_from_last_batch():
+    last_files = list(database.migrations.aggregate(
+        [
+            {
+                "$group" :
+                    {
+                        "_id": "$batch",
+                        "files": {"$addToSet": "$migration"},
+                    },
+            },
+            {
+                "$sort" : {"batch": -1}
+            }
+        ]
+    ))
+
+    if len(last_files) == 0:
+        return False
+
+    if len(last_files) > 0:
+        return last_files[0]
--- tests/test_generate_seeder_file.py
+++ tests/test_generate_seeder_file.py
@@ -1,10 +1,10 @@
 import os
 import os.path
 
-from generator import generate_migration_file
+from seeder_generator import generate_seeder
 
 
 def test_generate_migration_file():
-    file_name = generate_migration_file("file_test")
+    file_name = generate_seeder("file_test_seeder")
     return os.path.isfile(file_name)
 
