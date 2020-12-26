4fa5fa0b6497487e01e69faf4e6e234816e71638
Pedro Lazari <plazari96@gmail.com>
2020-12-26 15:31:54 -0300

    Ajustes

---
+++ tests/test_generate_file.py
@@ -0,0 +1,11 @@
+import os
+import os.path
+import sys
+
+from app.generator import generate_migration_file
+
+
+def test_generate_migration_file():
+    file_name = generate_migration_file("file_test")
+    return os.path.isfile(file_name)
+
